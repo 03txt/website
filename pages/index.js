1f77da6a690885ef0420f1477034a2650dafc3b4
dania <146884362+03txt@users.noreply.github.com>
2025-07-20 17:56:40 -0700

    changed title

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Head from 'next/head'
 import Header from '@components/Header'
-import Footer from '@components/Footer'
 
 export default function Home() {
   return (
@@ -11,13 +10,11 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="welcome to my website!" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
       </main>
-
-      <Footer />
     </div>
   )
 }
