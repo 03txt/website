ef0e88be9f074962acf00f2c6c958ebfab501898
dania <146884362+03txt@users.noreply.github.com>
2025-07-20 17:59:31 -0700

    added name

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
       <main>
         <Header title="welcome to my website!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          by <code>dania santiago</code>
         </p>
       </main>
     </div>
